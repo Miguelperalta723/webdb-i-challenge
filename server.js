a956449028b8f1af1690f8dd147a4338e8e0136c
Miguel Peralta <eudysperalta723@gmail.com>
2019-09-09 21:51:37 -0400

    mvp

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
-
+const accountsRouter = require('./accountsRouter')
 const db = require('./data/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
+server.use('/accounts', accountsRouter)
+
+
+server.get('/', (req, res) => {
+    res.send({message: 'api is up'})
+});
+
 
 module.exports = server;
